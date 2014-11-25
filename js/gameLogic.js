4a55030d4a902b29e6ffa3f142b31a77196b9bd8
KrishnaRSampath <Krishna.R.Sampath@gmail.com>
2014-11-25 14:22:37 -0500

    Separated out JS into its own file.

---
+++ js/gameLogic.js
@@ -0,0 +1,77 @@
+
+var filledBoxes;
+var content;
+
+var b;
+var squaresFilled = 0;
+var turn = 0;
+
+var context; //must set this for canvas
+
+var gameOver = false;
+var winningMoves;
+
+window.onload = function() {
+	filledBoxes = new Array();
+	content = new Array();
+	winningMoves = 
+		[[0,1,2],[3,4,5],[6,7,8],
+		 [0,3,6],[1,4,7],[2,5,8],
+		 [0,4,8],[2,4,6]];
+
+	for (i=0; i<9; i++) {
+		filledBoxes[i] = false;
+		content[i] = '';
+	}
+
+}
+
+
+function boxClicked(boxNumber) {
+	box = "box"+boxNumber;
+	b = document.getElementById(box);
+	context = b.getContext("2d");
+
+	if (gameOver == false) {
+		if(filledBoxes[boxNumber] == false) {
+			if(turn%2==0) {
+				context.fillStyle = "#000";
+				context.textAlign = "center";
+				context.font = "bold 150px Tahoma";
+				context.textBaseline = "top";
+				context.fillText("X", 150, -20);
+				content[boxNumber] = 'X';
+			}
+			else {
+			context.fillStyle = "#000";
+				context.textAlign = "center";
+				context.font = "bold 150px Tahoma";
+				context.textBaseline = "top";
+				context.fillText("O", 150, -20);
+				content[boxNumber] = 'O';	
+			}
+			turn++;
+			squaresFilled++;
+			filledBoxes[boxNumber] = true;
+		}
+
+		checkVictory(content[boxNumber]);
+
+	}
+}
+
+function checkVictory(playerIcon) {
+	for (var i = 0; i < winningMoves.length; i++) {
+		if ((content[winningMoves[i][0]]==playerIcon)&&(content[winningMoves[i][1]]==playerIcon)&&(content[winningMoves[i][2]]==playerIcon)) {
+			alert(playerIcon + " wins!");
+			gameOver = true;
+			location.reload(true);
+		} 
+	}
+	if ((gameOver==false)&&(turn == 9)) {
+		alert("Nobody wins!");
+		location.reload(true);
+	}
+}
+
+
